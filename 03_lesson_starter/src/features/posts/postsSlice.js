ee476f7b19109176e8d0c54e561e93fb827524c0
hailatGH <hailat.alx@gmail.com>
2022-10-27 17:03:51 +0300

    03 lesson done

--- 03_lesson_starter/src/features/posts/postsSlice.js
+++ 03_lesson_starter/src/features/posts/postsSlice.js
@@ -15,6 +15,11 @@ export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
     return response.data
 })
 
+export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPost) => {
+    const response = await axios.post(POSTS_URL, initialPost)
+    return response.data
+})
+
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
@@ -78,13 +83,36 @@ const postsSlice = createSlice({
                 state.status = 'failed'
                 state.error = action.error.message
             })
+            .addCase(addNewPost.fulfilled, (state, action) => {
+                // Fix for API post IDs:
+                // Creating sortedPosts & assigning the id 
+                // would be not be needed if the fake API 
+                // returned accurate new post IDs
+                const sortedPosts = state.posts.sort((a, b) => {
+                    if (a.id > b.id) return 1
+                    if (a.id < b.id) return -1
+                    return 0
+                })
+                action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
+                // End fix for fake API post IDs 
+
+                action.payload.userId = Number(action.payload.userId)
+                action.payload.date = new Date().toISOString();
+                action.payload.reactions = {
+                    thumbsUp: 0,
+                    hooray: 0,
+                    heart: 0,
+                    rocket: 0,
+                    eyes: 0
+                }
+                console.log(action.payload)
+                state.posts.push(action.payload)
+            })
     }
 })
 
 export const selectAllPosts = (state) => state.posts.posts;
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
-
 export const { postAdded, reactionAdded } = postsSlice.actions
-
 export default postsSlice.reducer
